ad2054e11e4fa8d687c0e0f97ec05e93fc5108f7
alexandrov5433 <alexandrov5433@gmail.com>
2026-01-15 18:13:50 +0100

    Binary tree init.

---
+++ include/data_structures/binary_tree.h
@@ -0,0 +1,70 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+#ifdef _WIN32
+#ifdef GLIB_EXPORTS
+#define GLIB_API __declspec(dllexport)
+#else
+#define GLIB_API __declspec(dllimport)
+#endif
+#else
+#define GLIB_API
+#endif
+
+typedef struct Node
+{
+    void *data;
+    struct Node *left, *right;
+} Node;
+
+typedef struct BinaryTree
+{
+    struct Node *root;
+    int (*comparator)(const void *a, const void *b);
+} BinaryTree;
+
+/**
+ * @param ptr The pointer, which will be the data of the root Node. Can not be a NULL pointer.
+ * @param comparator A function pointer to the comparator function, which will be used to order the Nodes. (<0) a comes BEFORE b. (0) a and b are EQUAL. (>0) a comes AFTER b. Can not be a NULL pointer.
+ * @returns A pointer to the new BinaryTree.
+ */
+GLIB_API BinaryTree *newBinaryTree(const void *ptr, int (*comparator)(const void *a, const void *b));
+
+/**
+ * Frees the memory for the BinaryTree. The data at the adsresses of the data pointers in the Nodes is not manipulated.
+ * @param bt A pointer to the BinaryTree, which must be freed. Can not be a NULL pointer.
+ * @returns 0 on succes. 1 the argument bt is NULL.
+ */
+GLIB_API int freeBT(BinaryTree *bt);
+
+/**
+ * Frees the memory for the BinaryTree and appies a releaser function on every data pointer in every Node.
+ * @param bt A pointer to the BinaryTree, which must be freed. Can not be a NULL pointer.
+ * @param releaser A function pointer to the releaser function, which will be applied to every data pointer. Can not be a NULL pointer, otherwise returns NULL.
+ * @returns 0 on succes. 1 if one of the arguments is NULL.
+ */
+GLIB_API int freeBTAndData(BinaryTree *bt, void (*releaser)(const void *ptr));
+
+/**
+ * @param bt A pointer to the BinaryTree, in which the new pointer must be added. Can not be a NULL pointer.
+ * @param ptr A pointer, which will be the the data of the newly added Node. Can not be a NULL pointer.
+ * @returns 0 on success. 1 when a NULL pointer was received as an argument. 2 when a new Node could not be created from the ptr argument. 3 on unknown error; the function should not be reaching this return statement.
+ */
+GLIB_API int addToBT(BinaryTree *bt, const void *ptr);
+
+/**
+ * @param bt A pointer to the BinaryTree, in which the new pointer must be added. Can not be a NULL pointer.
+ * @param searchAlg <=0 for Depth First Search (pre-order) and >0 for Breadth First Search.
+ * @param selector A function pointer to the comparator function, which will be used to find the Node containing the wanted data. Must return 1 if the currently processed data is the wanted one. Can not be a NULL pointer, otherwise returns NULL.
+ * @returns A pointer to the wanted data or NULL if nothing was found.
+ */
+GLIB_API void *findInBT(BinaryTree *bt, int searchAlg, int (*selector)(const void *ptr));
+
+/**
+ * Applies a processor function to every data pointer of every Node in the BinaryTree.
+ * @param bt A pointer to the BinaryTree, in which the new pointer must be added. Can not be a NULL pointer.
+ * @param selector A function pointer to the processor function. Can not be a NULL pointer, otherwise returns.
+ */
+GLIB_API void processBT(BinaryTree *bt, void (*processor)(const void *ptr));
+
+#endif
---
+++ src/data_structures/binary_tree.c
@@ -0,0 +1,194 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/data_structures/binary_tree.h"
+
+static Node *newNode(const void *data)
+{
+    if (data == NULL)
+    {
+        return NULL;
+    }
+
+    Node *node = calloc(1, sizeof(Node));
+    if (node == NULL)
+    {
+        return NULL;
+    }
+
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+BinaryTree *newBinaryTree(const void *ptr, int (*comparator)(const void *a, const void *b))
+{
+    if (ptr == NULL || comparator == NULL)
+    {
+        return NULL;
+    }
+
+    BinaryTree *bt = (BinaryTree *)malloc(sizeof(BinaryTree));
+    if (bt == NULL)
+    {
+        return NULL;
+    }
+
+    Node *root = newNode(ptr);
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    bt->root = root;
+    bt->comparator = comparator;
+
+    return bt;
+}
+
+static void freeNode(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    freeNode(node->left);
+    freeNode(node->right);
+    free(node);
+}
+
+int freeBT(BinaryTree *bt)
+{
+    if (bt == NULL)
+    {
+        return 1;
+    }
+    freeNode(bt->root);
+    free(bt);
+    return 0;
+}
+
+static void freeNodeAndData(Node *node, void (*releaser)(const void *ptr))
+{
+    if (node == NULL)
+    {
+        return;
+    }
+
+    freeNodeAndData(node->left, releaser);
+    freeNodeAndData(node->right, releaser);
+
+    releaser(node->data);
+    free(node);
+}
+
+int freeBTAndData(BinaryTree *bt, void (*releaser)(const void *ptr))
+{
+    if (bt == NULL || releaser == NULL)
+    {
+        return 1;
+    }
+    freeNodeAndData(bt->root, releaser);
+    free(bt);
+    return 0;
+}
+
+int addToBT(BinaryTree *bt, const void *ptr)
+{
+    if (bt == NULL || ptr == NULL)
+    {
+        return 1;
+    }
+
+    Node *newChildNode = newNode(ptr);
+    if (newChildNode == NULL)
+    {
+        return 2;
+    }
+
+    Node *node = bt->root;
+
+    while (1)
+    {
+        int result = (bt->comparator)(node->data, ptr);
+        //(<0) a comes BEFORE b. (0) a and b are EQUAL. (>0) a comes AFTER b.
+        if (result < 0)
+        {
+            // less -> left child
+            if (node->left == NULL)
+            {
+                // create Node from ptr; add as left child
+                node->left = newChildNode;
+                return 0;
+            }
+            // assign left child to node; continue cicle
+            node = node->left;
+        }
+        else
+        {
+            // equal or bigger -> right child
+            if (node->right == NULL)
+            {
+                // create Node from ptr; add as right child
+                node->right = newChildNode;
+                return 0;
+            }
+            // assign right child to node; continue cicle
+            node = node->right;
+        }
+    }
+
+    return 3;
+}
+
+static void *_DFS(Node *node, int (*selector)(const void *ptr))
+{
+    // depth first search; pre-order
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    if ((selector)(node->data) == 1)
+    {
+        return node->data;
+    }
+    return _DFS(node->left, selector) || _DFS(node->right, selector);
+}
+
+static void *_BFS(Node *node, int (*selector)(const void *ptr))
+{
+    // TODO
+}
+
+void *findInBT(BinaryTree *bt, int searchAlg, int (*selector)(const void *ptr))
+{
+    if (bt == NULL || selector == NULL)
+    {
+        return NULL;
+    }
+    if (searchAlg <= 0)
+    {
+        return _DFS(bt->root, selector);
+    }
+    return _BFS(bt->root, selector);
+}
+
+static void _processBT(Node *node, void (*processor)(const void *ptr))
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    _processBT(node->left, processor);
+    _processBT(node->right, processor);
+}
+
+void processBT(BinaryTree *bt, void (*processor)(const void *ptr))
+{
+    if (bt == NULL || processor == NULL)
+    {
+        return;
+    }
+    _processBT(bt->root, processor);
+}
