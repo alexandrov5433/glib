acf8153f8a3c98a4f49d9d09b11363a11956942b
alexandrov5433 <alexandrov5433@gmail.com>
2026-04-30 16:56:28 +0200

    Redux.

--- include/hash_map.h
+++ include/hash_map.h
@@ -45,6 +45,7 @@ enum HashMapError
     HM_ERR_KEY_COPY = 5,                    /**< (5) An error occured while coping the key using strcpy_s. */
     HM_ERR_FULL = 6,                        /**< (6) The HashMap is full. */
     HM_ERR_INVALID_ARGUMENT_DIMENTIONS = 7, /**< (7) The dimentions of one or more arguments, either alone or in their combination, do not match the expectations of the function. */
+    HM_ERR_DYNAMIC_ARRAY = 8,               /**< (8) A DynamicArray function returned a @ref DynamicArrayError error code. */
 };
 
 /**
@@ -70,93 +71,140 @@ typedef struct HashMap
 } HashMap;
 
 /**
- * Creates a new HashMap. The HashMap starts with an initial capacity of 10.
- * @returns A pointer to the HashMap. If memory could not be allocated, NULL is returned.
+ * Creates a new HashMap.
+ * @return A value of the @ref HashMapError:
+ * 
+ * - HM_SUCCESS
+ * 
+ * - HM_ERR_NULL_ARGUMENT
+ * 
+ * - HM_ERR_MEMORY_ALLOCATION
  */
-GALXLIB_API HashMap *new_hash_map();
+GALXLIB_API enum HashMapError new_hash_map(void (*value_destructor)(void *value), HashMap **const output);
 
 /**
- * Adds a destructor function to the HashMap. If one is already present, the old one is replaced with the new one.
- * The destructor receives every available Entry upon calling free_hash_map and is ment to free the memory of the value pointer, given by the user.
- * @param value_destructor A function pointer. Receives as a single argument an Entry pointer.
- * This function must NOT manipulate the Entry it self or its properties.
- * It sould simply free the memory pointer to by the value property pointer. The key preperty may be red, but not changed.
+ * Adds a destructor function to the HashMap. If one is already present, it is replaced with the new one.
+ * It is ment to free the memory of the value pointer, contained in the Entry.
  * @param map A pointer to the HashMap, in which the destructor must be added.
- * @returns 0 on success. 1 on failure, if either of the arguments are NULL.
+ * @param value_destructor A function pointer. Receives as a single argument - a void pointer.
+ * @return A value of the @ref HashMapError:
+ * 
+ * - HM_SUCCESS
+ * 
+ * - HM_ERR_NULL_ARGUMENT
  */
-GALXLIB_API int add_destructor_hm(void (*value_destructor)(const Entry *const entry), HashMap *map);
+GALXLIB_API enum HashMapError add_destructor_hm(HashMap *const map, void (*value_destructor)(void *value));
 
 /**
- * Frees the HashMap. The pointer of the keys and values are not freed.
+ * Frees the memory of the HashMap. The pointer of the keys and values are not freed. 
+ * If the HashMap containes a value_destructor, the value_destructor is invoked with every present value from each Entry.
  * @param map A pointer to the HashMap, which must be freed.
+ * @return A value of the @ref HashMapError:
+ * 
+ * - HM_SUCCESS
+ * 
+ * - HM_ERR_NULL_ARGUMENT
  */
-GALXLIB_API void free_hash_map(HashMap *map);
+GALXLIB_API enum HashMapError free_hash_map(HashMap *map);
 
 /**
- * Adds a new entry (key-value pair) to the HashMap.
+ * Adds a new Entry (key-value pair) to the HashMap. 
+ * If an Entry with the same key already exists, it is replaced with the new one.
+ * @param map A pointer to the HashMap, in which the new Entry must be added.
  * @param key A null-terminated string, which is copied to create the key for the given value.
  * @param value A pointer, which will be added as the value in the HashMap for the given key.
- * @param map A pointer to the HashMap, in which the new entry must be added.
- * @returns 0 on success;
- * 1 on failure due to lacking capacity;
- *
- * 2 on failure to expand the HashMap because of memory allocation failure;
- *
- * 22 on failure to due to lack of free space after extending it;
- *
- * 3 on failure to create a new Entry from the given key-value pair;
+ * @return A value of the @ref HashMapError:
+ * 
+ * - HM_SUCCESS
+ * 
+ * - HM_ERR_NULL_ARGUMENT
+ * 
+ * - HM_ERR_FULL
+ * 
+ * - HM_ERR_KEY_EMPTY
+ * 
+ * - HM_ERR_KEY_MAX_LENGTH
+ * 
+ * - HM_ERR_MEMORY_ALLOCATION
  */
-GALXLIB_API int put_hm(char *key, void *value, HashMap *map);
+GALXLIB_API enum HashMapError put_hm(HashMap *const map, char *const key, void *const value);
 
 /**
- * Returns the value for the given key in the HashMap.
- * @param key A null-terminated string, which is the key for the given value.
+ * Gets the value for the given key from the HashMap.
  * @param map A pointer to the HashMap, from which to find the value.
- * @returns The pointer for the given key. If not found, NULL is returned.
+ * @param key A null-terminated string, which is the key for the given value.
+ * @param output A pointer, where the value will be placed.
+ * @return A value of the @ref HashMapError:
+ * 
+ * - HM_SUCCESS
+ * 
+ * - HM_EMPTY
+ * 
+ * - HM_NOT_FOUND
+ * 
+ * - HM_ERR_NULL_ARGUMENT
+ * 
+ * - HM_ERR_KEY_EMPTY
+ * 
+ * - HM_ERR_KEY_MAX_LENGTH
  */
-GALXLIB_API void *get_hm(char *key, HashMap *map);
+GALXLIB_API enum HashMapError get_hm(const HashMap *const map, const char *const key, void **const output);
 
 /**
- * Removes an entry from the HashMap.
- * @param key A null-terminated string, which is the key of the entry.
- * @param map A pointer to the HashMap, from which the entry must be removed.
- * @returns 0 on success;
- * 1 on failure, because an entry with the given key was not found;
- *
- * 2 on failure due to memory allocation failure.
- *
- * 22 on failure due to lack of free space after shrinking it;
+ * Removes an Entry from the HashMap.
+ * @param map A pointer to the HashMap, from which the Entry must be removed.
+ * @param key A null-terminated string, which is the key of the Entry.
+ * @return A value of the @ref HashMapError:
+ * 
+ * - HM_SUCCESS
+ * 
+ * - HM_EMPTY
+ * 
+ * - HM_NOT_FOUND
+ * 
+ * - HM_ERR_NULL_ARGUMENT
+ * 
+ * - HM_ERR_KEY_EMPTY
+ * 
+ * - HM_ERR_KEY_MAX_LENGTH
+ * 
+ * - HM_ERR_MEMORY_ALLOCATION
  */
-GALXLIB_API int remove_hm(char *key, HashMap *map);
+GALXLIB_API enum HashMapError remove_hm(HashMap *const map, const char *const key);
 
 /**
  * Applies a processor function to every Entry in the HashMap.
- * @param processor A function pointer to the function, which will receive a pointer to every currently available Entry in the HashMap.
  * @param map A pointer to the HashMap, the Entries of which must be processed.
- * @returns 0 on success; 1 if either of the arguments are null pointers.
+ * @param processor A function pointer to the function, which will receive a pointer to every currently available Entry in the HashMap.
+ * @return A value of the @ref HashMapError:
+ * 
+ * - HM_SUCCESS
+ * 
+ * - HM_ERR_NULL_ARGUMENT
  */
-GALXLIB_API int process_hm(void (*processor)(Entry *const ptr), HashMap *const map);
+GALXLIB_API enum HashMapError process_hm(const HashMap *const map, void (*processor)(const Entry *const ptr));
 
 /**
  * Filters the Entries of the HashMap, leaving only the selected ones.
+ * @param map A pointer to the HashMap, which must be filtered.
  * @param selector A function pointer to the function, which will receive a pointer to every currently available Entry in the HashMap.
  * The selector returns 1 if the Entry must STAY, 0 if it must be REMOVED.
- * @param map A pointer to the HashMap, which must be filtered.
- * @returns 0 on success;
- * 1 if either of the arguments are null pointers;
- *
- * 2 if memory could not be allocated for temporary storage;
- *
- * 30-something if the Entry pointer could not be placed in the DynamicArray for temporary storage by the push_da function.
- * The error codes of push_da are the second digit: in 31 the error code of push_da is 1.
- * The error code is created by adding the error code from push_da to 30.
- *
- * 3 if at_da function returned with an error code > 0, while trying to get an Entry pointer element from the DynamicArray temporary storage;
- *
- * 4 if the Entry pointer element from the DynamicArray temporary storage, returned from at_da, is NULL;
- *
- * 5 if the selected Entry could not be incerted in the new Entries array due to lacking capacity;
+ * @return A value of the @ref HashMapError:
+ * 
+ * - HM_SUCCESS
+ * 
+ * - HM_ERR_DYNAMIC_ARRAY
+ * 
+ * - HM_ERR_NULL_ARGUMENT
+ * 
+ * - HM_ERR_MEMORY_ALLOCATION
+ * 
+ * - HM_ERR_FULL
+ * 
+ * - HM_ERR_KEY_EMPTY
+ * 
+ * - HM_ERR_KEY_MAX_LENGTH
  */
-GALXLIB_API int filter_hm(int (*selector)(Entry *const ptr), HashMap *const map);
+GALXLIB_API enum HashMapError filter_hm(HashMap *const map, int (*selector)(const Entry *const ptr));
 
 #endif
--- src/hash_map.c
+++ src/hash_map.c
@@ -51,7 +51,7 @@ static enum HashMapError _free_entry(Entry *entry)
 	return HM_SUCCESS;
 }
 
-static enum HashMapError _hash_str(const char *str, const size_t hm_capacity, size_t *const output)
+static enum HashMapError _hash_str(const char *const str, const size_t hm_capacity, size_t *const output)
 {
 	if (str == NULL || output == NULL)
 		return HM_ERR_NULL_ARGUMENT;
@@ -106,7 +106,7 @@ static enum HashMapError _incert_replace_entry(
     const size_t hm_capacity,
     Entry **const hm_entries,
     int *const output_is_replaced,
-    void (*value_destructor)(const Entry *const entry))
+    void (*value_destructor)(void *value))
 {
 	// value_destructor CAN be NULL!
 	if (new_entry == NULL || hm_entries == NULL || output_is_replaced == NULL)
@@ -283,84 +283,108 @@ enum HashMapError add_destructor_hm(HashMap *const map, void (*value_destructor)
 	return HM_SUCCESS;
 }
 
-void free_hash_map(HashMap *map)
+enum HashMapError free_hash_map(HashMap *map)
 {
 	if (map == NULL)
-	{
-		return;
-	}
-	for (int i = 0; i < map->capacity; ++i)
+		return HM_ERR_NULL_ARGUMENT;
+
+	for (size_t i = 0; i < map->capacity; ++i)
 	{
 		Entry *e = (map->entries)[i];
 		if (e == NULL)
 			continue;
 		if (map->value_destructor != NULL)
-			(map->value_destructor)(e);
+			(map->value_destructor)(e->value);
 		_free_entry(e);
 	}
 	free(map->entries);
 	free(map);
+
+	return HM_SUCCESS;
 }
 
-int put_hm(char *key, void *value, HashMap *map)
+enum HashMapError put_hm(HashMap *const map, char *const key, void *const value)
 {
-	switch (_extend_hm(map))
-	{
-	case 1:
-		return 2;
-	case 2:
-		return 22;
-	}
+	if (map == NULL || key == NULL || value == NULL)
+		return HM_ERR_NULL_ARGUMENT;
 
-	Entry *newEntry = _new_entry(key, value);
-	if (newEntry == NULL)
-		return 3;
+	int err_extend = _extend_hm(map);
+	if (err_extend)
+		return err_extend;
 
-	int isReplaced = 0;
-	if (_incert_replace_entry(newEntry, map->capacity, map->entries, &isReplaced, map->value_destructor))
-		return 1;
+	Entry *new_entry = NULL;
+	int err_new_entry = _new_entry(key, value, &new_entry);
+	if (err_new_entry)
+		return err_new_entry;
+
+	int is_replaced = 0;
+	int err_incert = _incert_replace_entry(new_entry, map->capacity, map->entries, &is_replaced, map->value_destructor);
+	if (err_incert)
+		return err_incert;
 
 	// if 0, Entry was added. If 1, it replaced another, older one with the same key.
-	if (isReplaced == 0)
+	if (is_replaced == 0)
 		(map->n_ent)++;
 
-	return 0;
+	return HM_SUCCESS;
 }
 
-void *get_hm(char *key, HashMap *map)
+enum HashMapError get_hm(const HashMap *const map, const char *const key, void **const output)
 {
-	size_t index = _hash_str(key, map->capacity);
-	for (size_t i = index; i < map->capacity; i++)
+	if (map == NULL || key == NULL || output == NULL)
+		return HM_ERR_NULL_ARGUMENT;
+
+	if (map->n_ent == 0)
+		return HM_EMPTY;
+
+	size_t index = 0;
+	int err_hash = _hash_str(key, map->capacity, &index);
+	if (err_hash)
+		return err_hash;
+
+	for (size_t i = index; i < map->capacity; ++i)
 	{
 		Entry *ent = (map->entries)[i];
 		if (ent == NULL)
 			continue;
 		if (strcmp(key, ent->key) == 0)
-			return ent->value;
+		{
+			*output = ent->value;
+			return HM_SUCCESS;
+		}
 	}
-	for (size_t i = 0; i < index; i++)
+	for (size_t i = 0; i < index; ++i)
 	{
 		Entry *ent = (map->entries)[i];
 		if (ent == NULL)
 			continue;
 		if (strcmp(key, ent->key) == 0)
-			return ent->value;
+		{
+			*output = ent->value;
+			return HM_SUCCESS;
+		}
 	}
-	return NULL;
+	return HM_NOT_FOUND;
 }
 
-int remove_hm(char *key, HashMap *map)
+enum HashMapError remove_hm(HashMap *const map, const char *const key)
 {
-	switch (_squish_hm(map))
-	{
-	case 1:
-		return 2;
-	case 2:
-		return 22;
-	}
+	if (map == NULL || key == NULL)
+		return HM_ERR_NULL_ARGUMENT;
 
-	size_t index = _hash_str(key, map->capacity);
-	for (size_t i = index; i < map->capacity; i++)
+	if (map->n_ent == 0)
+		return HM_EMPTY;
+
+	int err_squish = _squish_hm(map);
+	if (err_squish)
+		return err_squish;
+
+	size_t index = 0;
+	int err_hash = _hash_str(key, map->capacity, &index);
+	if (err_hash)
+		return err_hash;
+
+	for (size_t i = index; i < map->capacity; ++i)
 	{
 		Entry *ent = (map->entries)[i];
 		if (ent == NULL)
@@ -369,15 +393,15 @@ int remove_hm(char *key, HashMap *map)
 		if (strcmp(key, ent->key) == 0)
 		{
 			if (map->value_destructor != NULL)
-				(map->value_destructor)(ent);
+				(map->value_destructor)(ent->value);
 
 			_free_entry(ent);
 			(map->entries)[i] = NULL;
 			(map->n_ent)--;
-			return 0;
+			return HM_SUCCESS;
 		}
 	}
-	for (size_t i = 0; i < index; i++)
+	for (size_t i = 0; i < index; ++i)
 	{
 		Entry *ent = (map->entries)[i];
 		if (ent == NULL)
@@ -386,67 +410,71 @@ int remove_hm(char *key, HashMap *map)
 		if (strcmp(key, ent->key) == 0)
 		{
 			if (map->value_destructor != NULL)
-				(map->value_destructor)(ent);
+				(map->value_destructor)(ent->value);
 
 			_free_entry(ent);
 			(map->entries)[i] = NULL;
 			(map->n_ent)--;
-			return 0;
+			return HM_SUCCESS;
 		}
 	}
-	return 1;
+	return HM_NOT_FOUND;
 }
 
-int process_hm(void (*processor)(Entry *const ptr), HashMap *const map)
+enum HashMapError process_hm(const HashMap *const map, void (*processor)(const Entry *const ptr))
 {
 	if (map == NULL || processor == NULL)
-		return 1;
+		return HM_ERR_NULL_ARGUMENT;
 
 	for (size_t i = 0; i < map->capacity; ++i)
 	{
-		Entry *const e = map->entries[i];
+		const Entry *const e = map->entries[i];
 		if (e != NULL)
 			processor(e);
 	}
 
-	return 0;
+	return HM_SUCCESS;
 }
 
-int filter_hm(int (*selector)(Entry *const ptr), HashMap *const map)
+enum HashMapError filter_hm(HashMap *const map, int (*selector)(const Entry *const ptr))
 {
 	if (map == NULL || selector == NULL)
-		return 1;
+		return HM_ERR_NULL_ARGUMENT;
+
+	if (map->n_ent == 0)
+		return HM_SUCCESS;
+
+	enum HashMapError error_code = HM_SUCCESS;
 
 	DynamicArray *entries_to_keep = NULL;
-	int err_entries_to_keep = new_dynamic_array(VOID_PTR, &entries_to_keep);
-	if (err_entries_to_keep)
-		return 2;
+	if (new_dynamic_array(VOID_PTR, &entries_to_keep))
+	{
+		error_code = HM_ERR_DYNAMIC_ARRAY;
+		goto _error_case;
+	}
 
 	DynamicArray *entreis_to_estroy = NULL;
 	if (map->value_destructor != NULL)
 	{
-		int err_entries_to_destroy = new_dynamic_array(VOID_PTR, &entreis_to_estroy);
-		if (err_entries_to_destroy)
+		if (new_dynamic_array(VOID_PTR, &entreis_to_estroy))
 		{
-			free_dynamic_array(entries_to_keep);
-			return 2;
+			error_code = HM_ERR_DYNAMIC_ARRAY;
+			goto _error_case;
 		}
 	}
 
 	for (size_t i = 0; i < map->capacity; ++i)
 	{
-		Entry *const e = map->entries[i];
+		const Entry *const e = map->entries[i];
 		if (e == NULL)
 			continue;
 
 		if (selector(e))
 		{
-			int push_err = push_ptr_da(entries_to_keep, e);
-			if (push_err > 0)
+			if (push_ptr_da(entries_to_keep, e))
 			{
-				free_dynamic_array(entries_to_keep);
-				free_dynamic_array(entreis_to_estroy);
-				return 30 + push_err;
+				error_code = HM_ERR_DYNAMIC_ARRAY;
+				goto _error_case;
 			}
 		}
 		else
@@ -454,24 +482,19 @@ int filter_hm(int (*selector)(Entry *const ptr), HashMap *const map)
 			if (map->value_destructor == NULL)
 				continue;
 
-			int push_err = push_ptr_da(entreis_to_estroy, e);
-			if (push_err > 0)
+			if (push_ptr_da(entreis_to_estroy, e))
 			{
-				free_dynamic_array(entries_to_keep);
-				free_dynamic_array(entreis_to_estroy);
-				return 30 + push_err;
+				error_code = HM_ERR_DYNAMIC_ARRAY;
+				goto _error_case;
 			}
 		}
 	}
 
-	size_t newEntriesCapacity = entries_to_keep->count + HASH_MAP_INIT_CAPACITY;
-	Entry **newEntries = _new_entries_array(newEntriesCapacity);
-	if (newEntries == NULL)
-	{
-		free_dynamic_array(entries_to_keep);
-		free_dynamic_array(entreis_to_estroy);
-		return 2;
-	}
+	size_t new_entries_capacity = entries_to_keep->count + HASH_MAP_INIT_CAPACITY;
+	Entry **new_entries = NULL;
+	int err_new_entries = _new_entries_array(new_entries_capacity, &new_entries);
+	if (err_new_entries)
+		return err_new_entries;
 
 	// Case: all Entries must be removed from the HashMap.
 	if (entries_to_keep->count == 0)
@@ -480,29 +503,27 @@ int filter_hm(int (*selector)(Entry *const ptr), HashMap *const map)
 	// Case: leave selected Entries.
 	for (size_t i = 0; i < entries_to_keep->count; ++i)
 	{
-		Entry *currentEntry = NULL;
+		Entry *current_entry = NULL;
 
-		int atErr = at_da(entries_to_keep, i, (void **)&currentEntry);
-		if (atErr)
+		int err_at = at_da(entries_to_keep, i, (void **)&current_entry);
+		if (err_at != DA_SUCCESS && err_at != DA_ARRAY_EMPTY)
 		{
-			free_dynamic_array(entries_to_keep);
-			free_dynamic_array(entreis_to_estroy);
-			return 3;
+			error_code = HM_ERR_DYNAMIC_ARRAY;
+			goto _error_case;
 		}
 
-		if (currentEntry == NULL)
-		{
-			free_dynamic_array(entries_to_keep);
-			free_dynamic_array(entreis_to_estroy);
-			return 4;
-		}
+		// if (current_entry == NULL)
+		// {
+		// 	free_dynamic_array(entries_to_keep);
+		// 	free_dynamic_array(entreis_to_estroy);
+		// 	return 4;
+		// }
 
-		int incertErr = _incert_entry(currentEntry, newEntriesCapacity, newEntries);
-		if (incertErr)
+		int err_incert = _incert_entry(current_entry, new_entries_capacity, new_entries);
+		if (err_incert)
 		{
-			free_dynamic_array(entries_to_keep);
-			free_dynamic_array(entreis_to_estroy);
-			return 5;
+			error_code = err_incert;
+			goto _error_case;
 		}
 	}
 
@@ -511,31 +532,39 @@ _hashmap_manipulation_stage:
 	{
 		for (size_t i = 0; i < entreis_to_estroy->count; ++i)
 		{
-			Entry *currentEntry = NULL;
+			Entry *current_entry = NULL;
 
-			int atErr = at_da(entreis_to_estroy, i, (void **)&currentEntry);
-			if (atErr)
+			int err_at = at_da(entreis_to_estroy, i, (void **)&current_entry);
+			if (err_at != DA_SUCCESS && err_at != DA_ARRAY_EMPTY)
 			{
-				free_dynamic_array(entries_to_keep);
-				free_dynamic_array(entreis_to_estroy);
-				free(newEntries);
-				return 3;
+				error_code = HM_ERR_DYNAMIC_ARRAY;
+				goto _error_case;
 			}
 
-			if (currentEntry == NULL)
+			if (current_entry == NULL)
 				continue;
 
-			(map->value_destructor)(currentEntry);
+			(map->value_destructor)(current_entry->value);
 		}
 	}
 
 	free(map->entries);
-	map->entries = newEntries;
-	map->capacity = newEntriesCapacity;
+	map->entries = new_entries;
+	map->capacity = new_entries_capacity;
 	map->n_ent = entries_to_keep->count;
 
 	free_dynamic_array(entries_to_keep);
 	free_dynamic_array(entreis_to_estroy);
 
-	return 0;
+	return error_code;
+
+_error_case:
+	if (new_entries != NULL)
+		free(new_entries);
+	if (entries_to_keep != NULL)
+		free_dynamic_array(entries_to_keep);
+	if (entreis_to_estroy != NULL)
+		free_dynamic_array(entreis_to_estroy);
+
+	return error_code;
 }
